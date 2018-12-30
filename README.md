e255b1c982caba94b8649efa6faa5506f1f045a1
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-29 18:44:16 -0600

    misc js and updated README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-Grady's Daily Journal
+# Grady's Daily Journal
+
+### borrowed from Colleen Woolsey's README:
+1. 1. DAILY JOURNAL PART 1 AND 2
+
+Built a form for entering in journal entries.
+Defined how the data collected in the form would be stored.
+Built a data structure to represent a journal entry.
+Built a function that returns an HTML representation of that data structure.
+Rendered it to the DOM.
+1. DAILY JOURNAL - PART 3 The learning objectives were:
+
+To set up JSON Server to store the data for daily journal entries
+Query that data from the API
+Use Promises (fetch)
+Render the entries to the DOM.
+1. DAILY JOURNAL - PART 4
+
+Modularized JavaScript code - created data.js, entriesDOM.js and entryComponent.js
+Refactored journal.js and included new script tags in index.html in correct order
+OPTIONAL CHALLENGE
+
+Change the code in both src/scripts/entriesDOM.js and src/scripts/entryComponent.js so that the functions in each one becomes a method on an object
+Once the objects are defined, refactor your code to use the methods on those objects where needed
+1. DAILY JOURNAL - PART 5
+
+Use event listeners, and querying the DOM to make your daily journal form work
+Save entries to your API
+Use fetch to make a POST request to your API
+OPTIONAL CHALLENGE A: Advanced Form Validation Add the following validation code to your journal entry form fields
+
+Pick a maximum length for concepts field and provide visual feedback if you type in a string that is longer than that maximum
+Test that the concept and entry fields contain no curse words. You can use regular expressions for that OPTIONAL CHALLENGE B: Entry Form Component
+Create a module that defines a function for building the form fields dynamically instead of them being hard-coded in the HTML OPTIONAL CHALLENGE C: Creating Method Abstractions
+Write a method in your API module that performs a POST and a GET, then use that method in the event listener.
--- scripts/journal.js
+++ scripts/journal.js
@@ -22,9 +22,9 @@ formContainer.innerHTML = `<form>
 <section>
 <label for="mood">Mood:</label>
 <select id="mySelect" name="mood" size="4">
-                <option value="happy">Happy</option>
-                <option value="fine">Fine</option>
-                <option value="sad">Sad</option>
+                <option value="Happy">Happy</option>
+                <option value="Fine">Fine</option>
+                <option value="Sad">Sad</option>
                 <option value="Anxious">Anxious</option>
         </select>
 </section>
@@ -41,6 +41,7 @@ articlesContainer.classList.add("articles-container");
 let resourcesContainer = document.querySelector("#resources-container");
 resourcesContainer.appendChild(articlesContainer);
 
+let containerRef = document.querySelector("#container");
 
 
 const journalEntries = [
@@ -49,63 +50,86 @@ const journalEntries = [
         concept: "Array methods",
         entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
         mood: "Ok"
+    },
+    {
+        date: "08/24/2018",
+        concept: "more Array methods",
+        entry: "bloop bloop bleep",
+        mood: "Ok"
     }
 ]
 
 // console.table(journalEntries);
 
+
 // copy pasta from Jisie's Resource Tracker
 let createAndAppendResources = () => {
-    for(let i = 0; i < journalEntries.length; i++){
-      console.log(journalEntries[i]);
+    // for(let i = 0; i < journalEntries.length; i++){
+    //     console.log(journalEntries[i]);
+    // }
+    // articlesContainer.innerHTML = "";
+
+    // for(let i = 0; i < journalEntries.length; i++){
+
+        for (entry of journalEntries) {
+        // console.log(`entry ${i}: `, entry)
+        let entryComponent = `
+        <div id="entry" class="">
+        <h1${entry.concept} >
+        <section${entry.entry} >
+        <aside${entry.date} >
+        </div>
+    `
+
+            articlesContainer.innerHTML += entryComponent;
+
+        }
     }
-    articlesContainer.innerHTML = "";
-  
-    journalEntries.forEach(function(resourceObj){
-      // console.log(resourceObj);
-      const resourceElement = document.createElement("section");
-      // <a href={link}>{name}</a>
-      // const linkElement = document.createElement("a");
-      // linkElement.textContent = resourceObj.name;
-      // linkElement.setAttribute("href", resourceObj.link);
-      // resourceElement.appendChild(linkElement);
-      
-      articlesContainer.appendChild(resourceElement);
-   
-    })
-  }
-  
-  createAndAppendResources();
-
-
-  let addButton = document.querySelector("#add-button");
-  addButton.addEventListener("click", () => {
-    console.log("Button was clicked");
-  
-    let resourceName = document.querySelector('input[name="date"]').value;
-    let resourceLink = document.querySelector('input[name="conceptscovered"]').value;
-    let resourceLink2 = document.querySelector('input[name="journalentry"]').value;
-    // i'm not able to retrieve the value of the below so i am commenting out for more research
-    
-        let x = document.getElementById("mySelect").selectedIndex;
-        let y = document.getElementsByTagName("option")[x].value;
-    
-    // console.log("Input values: ", resourceName, resourceLink, resourceLink2, resourceType);
-    console.log("Input values: ", resourceName, resourceLink, resourceLink2, y);
-  
-    let resource = {
-      date: resourceName,
-      concept: resourceLink,
-      entry: resourceLink2,
-      mood: y
-    }
-  
-
-    journalEntries.push(resource);
     createAndAppendResources();
-  })
-  
-  
+
+    //event listener on add button; capture values from input fields
+    let addButton = document.querySelector("#add-button");
+    addButton.addEventListener("click", () => {
+        // console.log("Button was clicked");
+
+        //capture values from the form
+        let dateValue = document.querySelector('input[name="date"]').value;
+        let conceptsCoveredValue = document.querySelector('input[name="conceptscovered"]').value;
+        let journalEntryValue = document.querySelector('input[name="journalentry"]').value;
+        //for "mood" selector:
+        let moodSelector = document.getElementById("mySelect").selectedIndex;
+        let moodValue = document.getElementsByTagName("option")[moodSelector].value;
+
+        console.log("Input values: ", dateValue, conceptsCoveredValue, journalEntryValue, moodValue);
+
+        let resource = {
+            date: dateValue,
+            concept: conceptsCoveredValue,
+            entry: journalEntryValue,
+            mood: moodValue
+        }
+
+        console.log("resource: " , resource)
+        journalEntries.push(resource);
+
+        console.table("journalEntries ", journalEntries)
+        createAndAppendResources();
+    })
+
+
+
+//     for (entry of journalEntries) {
+//         // console.log(entry)
+//         let entryComponent = `
+//         <div id="entry" class="">
+//         <h1${entry.concept} >
+//         <section${entry.entry} >
+//         <aside${entry.date} >
+//         </div>
+//     `
+//     console.log("entryComponent" + entryComponent)
+//     containerRef.innerHTML += entryComponent
+// }
 
 
 
@@ -120,8 +144,8 @@ let createAndAppendResources = () => {
 
 // const makeJournalEntryComponent = (journalEntries) => {
 //     // Create your own HTML structure for a journal entry
-//     return 
-//     `<div class="entriesDOM"> 
+//     return
+//     `<div class="entriesDOM">
 //     <h2>${journalEntries.title}</h2>
 //     <h4>${journalEntries.date}</h4>
 //     <p>${journalEntries.content}</p>
